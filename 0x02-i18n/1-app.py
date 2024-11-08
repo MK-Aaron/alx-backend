0160244f3f366b9a4ab5d3cb27cc6090421820ad
Mk-Aaron <mwendwa_aaron78@yahoo.com>
2024-11-08 04:49:06 -0800

    Modified

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
-""""
-Simple flask app
-"""
+"""A Basic Flask application"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
-
-class config(object):
-    """Class config"""
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app = Flask(__name__)
-app.config.from_object(config)
-app.url_map.strict_slashes = False
 
 babel = Babel(app)
 
 
+class Config:
+    """Set configuration variables for the Flask-Babel extension."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
 @app.route('/')
-def home():
-    """Home page"""
-    return render_template('1-index.html')
+def index():
+    """Renders the index template."""
+    render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=TRue)
+    app.run(host='0.0.0.0', port=5000)
